8debd27a0a50e9491cf6800dc0c9e23a2cc53691
Heitor <0000877673@senaimgaluno.com.br>
2025-07-06 20:30:23 -0300

    feature/pagina-eventos

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,20 +13,8 @@ export default function Home() {
           arquivo <span className="">README.md</span>
         </p>
         <div className="flex gap-4 mt-8">
-          <Link
-            href="/clientes"
-            className="px-6 py-2 bg-blue-600 text-white rounded hover:bg-gray-700 transition"
-          >
-            Ir para Clientes
-          </Link>
-          <Link
-            href="/produtos"
-            className="px-6 py-2 bg-blue-600 text-white rounded hover:bg-gray-700 transition"
-          >
-            Ir para Produtos
-          </Link>
            <Link
-            href="/produtos"
+            href="/eventos"
             className="px-6 py-2 bg-blue-600 text-white rounded hover:bg-gray-700 transition"
           >
             Ir para Eventos
